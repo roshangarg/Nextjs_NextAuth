683cb3178c2d3a6cd6660772d7cde6869d0fda8c
roshan280601 <roshan280601@gmail.com>
2023-05-31 12:44:55 +0530

    deploy check2

--- components/sidebar.js
+++ components/sidebar.js
@@ -5,18 +5,18 @@ import { AiOutlineSchedule, AiFillSetting } from "react-icons/ai";
 import { BiUserCircle } from "react-icons/bi";
 const Sidebar = () => {
   return (
-    <div className="hidden md:flex flex-col h-100vh bg-black p-5 px-6 rounded-xl w-60 m-2 mr-5 ">
+    <div className="hidden md:flex flex-col h-100vh bg-black p-5 px-6 rounded-xl w-60 m-2 mr-5 text-white ">
       <div className="">
-        <h1 className="font-sans text-3xl font-bold leading-11 tracking-normal text-left text-left mt-5">
+        <h1 className="font-sans text-white text-3xl font-bold leading-11 tracking-normal text-left text-left mt-5">
           Board.
         </h1>
       </div>
-      <div className="flex-grow">
+      <div className="flex-grow text-white">
         <ul className="flex flex-col mt-8">
           <li className="py-2 ">
             <a
               href="#"
-              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left"
+              className="flex items-center text-white font-sans text-md font-bold leading-5 tracking-normal text-left"
             >
               <svg
                 className="w-6 h-6 mr-2 mt-2"
@@ -33,7 +33,7 @@ const Sidebar = () => {
           <li className="py-2">
             <a
               href="#"
-              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left"
+              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left text-white"
             >
               <svg
                 className="w-6 h-6 mr-2 mt-2"
@@ -50,7 +50,7 @@ const Sidebar = () => {
           <li className="py-2">
             <a
               href="#"
-              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left"
+              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left text-white"
             >
               <svg
                 className="w-6 h-6 mr-2 mt-2"
@@ -67,7 +67,7 @@ const Sidebar = () => {
           <li className="py-2">
             <a
               href="#"
-              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left"
+              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left text-white"
             >
               <svg
                 className="w-6 h-6 mr-2 mt-2"
@@ -84,7 +84,7 @@ const Sidebar = () => {
           <li className="py-2 ">
             <a
               href="#"
-              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left"
+              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left text-white"
             >
               <svg
                 className="w-6 h-6 mr-2 mt-2"
@@ -105,7 +105,7 @@ const Sidebar = () => {
           <li className="py-2 ">
             <a
               href="#"
-              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left"
+              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left text-white"
             >
               Help
             </a>
@@ -113,7 +113,7 @@ const Sidebar = () => {
           <li className="py-2 ">
             <a
               href="#"
-              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left"
+              className="flex items-center font-sans text-md font-bold leading-5 tracking-normal text-left text-white"
             >
               Contact
             </a>
